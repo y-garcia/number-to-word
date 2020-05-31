8c016d7d976a104952bb5e239204f208caec234f
Yeray García Quintana <64692593+y-garcia@users.noreply.github.com>
2020-05-31 12:12:10 +0200

    feat: Update major system characters

--- majorsystem.py
+++ majorsystem.py
@@ -2,26 +2,18 @@ class MajorSystem:
 
     def __init__(self):
         self._to_char = {
-            '0': ['z', 's', 'x'],
+            '0': ['z', 's', 'x', 'c'],
             '1': ['t', 'd'],
-            '2': ['n'],
+            '2': ['n', 'ñ'],
             '3': ['m'],
             '4': ['r'],
             '5': ['l'],
             '6': ['j', 'g'],
-            '7': ['k', 'c'],
+            '7': ['k', 'c', 'q'],
             '8': ['f'],
-            '9': ['b', 'p'],
-            '': ['a', 'e', 'i', 'o', 'u', 'y', 'h']
+            '9': ['b', 'p', 'v'],
+            '': ['a', 'e', 'i', 'o', 'u', 'y', 'h', 'w', 'á', 'é', 'í', 'ó', 'ú', 'ü']
         }
-        self._to_digit = {}
-        for number, chars in self._to_char.items():
-            for char in chars:
-                self._to_digit[char] = number
-        self.count = 0
-
-    def to_digit(self, char):
-        return self._to_digit[char]
 
     def to_chars(self, digit):
         return self._to_char[str(digit)]
